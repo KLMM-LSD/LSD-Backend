9dff71d89e97a0df792f5cd6134d4082aafbaf17
Michael <Michael_veilis@hotmail.com>
2018-09-14 16:09:38 +0200

    Har tilføjet PresentationLayer, LogicLayer, Entities og DBLayer mapper med tilhørende klasser

---
+++ LSDProjectBackend/src/main/java/entities/EntityClasse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Micha
+ */
+public class EntityClasse {
+    
+//    Entity klasser skal laves her, men kan først gøres når vi har en virkende Database!
+//    Entity klasser er:
+//    UserEntity
+//    DiscussionEntity
+//    Comment
+    
+    
+}
--- LSDProjectBackend/src/main/java/presentationLayer/FrontendConnector.java
+++ LSDProjectBackend/src/main/java/presentationLayer/FrontendConnector.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package redditHandler;
+package presentationLayer;
 
 /**
  *
  * @author Micha
  */
-public class Main {
+public class FrontendConnector {
     
 }
