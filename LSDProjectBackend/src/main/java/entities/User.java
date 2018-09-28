9c30361c088b9ac22466626047cbc975c59de060
Mart_ <Mart_@LAPTOP-84J82GVA.home>
2018-09-28 20:20:06 +0200

    Added getter and setters for User entity

--- LSDProjectBackend/src/main/java/entities/User.java
+++ LSDProjectBackend/src/main/java/entities/User.java
@@ -23,6 +23,38 @@ public class User {
         this.name = name;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
     
     
     
