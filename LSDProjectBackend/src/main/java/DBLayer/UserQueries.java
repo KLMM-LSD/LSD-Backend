f9e7538315ed1fad6f703c61924539c28fa21695
Mart_ <Mart_@LAPTOP-84J82GVA.home>
2018-09-29 20:58:26 +0200

    Added Queries for each, Finished User queries

--- LSDProjectBackend/src/main/java/DBLayer/UserQueries.java
+++ LSDProjectBackend/src/main/java/DBLayer/UserQueries.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class QueryFormer {
+public class UserQueries {
 
     private static DatabaseAccess access;
     private static Connection connection;
@@ -29,13 +29,13 @@ public class QueryFormer {
         pstmt.setString(4, user.getUserpassword());
         pstmt.execute();
     }
-    
+
     public static Users getUser(Connection con, int userid) throws ClassNotFoundException, SQLException {
         Users user = new Users(0L);
         Statement stmt = con.createStatement();
         String query = "SELECT * FROM users WHERE userid = " + userid + ";";
         ResultSet rs = stmt.executeQuery(query);
-        while(rs.next()){
+        while (rs.next()) {
             user.setUsertype(rs.getString(1));
             user.setUsername(rs.getString(2));
             user.setUserpassword(rs.getString(3));
@@ -43,10 +43,22 @@ public class QueryFormer {
         return user;
     }
 
+    public static void updateUser(Connection con, int userid, String password) throws SQLException {
+        PreparedStatement pstmt = con.prepareStatement("UPDATE Users SET userpassword = ? WHERE userid = " + userid + ";");
+        pstmt.setString(1, password);
+        pstmt.execute();
+    }
+    
+    public static void deleteUser(Connection con, int userid) throws SQLException{
+        PreparedStatement pstmt = con.prepareStatement("DELETE FROM Users WHERE userid = ?");
+        pstmt.setInt(1, userid);
+        pstmt.execute();
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         setUp();
 //        Users user = new Users("user", 0l, "Hans", "123");
 //        createUser(connection, "lsd", user);
-        getUser(connection, 1);
+        deleteUser(connection, 1);
     }
 }
