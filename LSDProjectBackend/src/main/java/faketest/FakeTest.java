4830a92a3f27fd468e702175689606d76050b8b3
unknown <lassehansencphbusiness@gmail.com>
2018-10-25 12:41:28 +0200

    Ordentlig refactor, pt. 5

--- LSDProjectBackend/src/main/java/faketest/FakeTest.java
+++ LSDProjectBackend/src/main/java/faketest/FakeTest.java
@@ -10,6 +10,7 @@ import DBLayer.UserQueries;
 import entities.Post;
 import entities.User;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -26,18 +27,34 @@ public class FakeTest {
         User u2 = new User(2, "user", System.currentTimeMillis(),
                 "einstein", "dasdas", "I am clever");
         
-        Post p1 = new Post(1, "story", System.currentTimeMillis(), 1, "Quite a story");
-        Post p2 = new Post(2, "comment", 1, System.currentTimeMillis(), 2, 1, "GOod story");
+        Post p1 = new Post(1, "story", System.currentTimeMillis(), 1, "Story Quite a story");
+        Post p2 = new Post(2, "comment", 1, System.currentTimeMillis(), 2, 1, "Comment GOod story");
+        Post p3 = new Post(3, "comment", 2, System.currentTimeMillis(), 1, 1, "Comment Thanks dude");
+        
+        Post p4 = new Post(4, "story", System.currentTimeMillis(), 2, "Story I am drunk");
+        Post p5 = new Post(5, "comment", 4, System.currentTimeMillis(), 1, 4, "Comment GOod story");
         
         uq.insertUser(u1);
         uq.insertUser(u2);
         
         pq.insertStory(p1);
         pq.insertPost(p2);
+        pq.insertPost(p3);
+        
+        pq.insertStory(p4);
+        pq.insertPost(p5);
         
     }
     
+    public static void testGet() throws SQLException {
+        PostQueries pq = new PostQueries();
+        ArrayList<Post> ret = pq.getThread(1);
+        
+        for (Post p : ret)
+            System.out.println(p.postcontent);
+    }
+    
     public static void main(String[] args) throws SQLException {
-        initUsers();
+        testGet();
     }
 }
