6279b61fb23571d793ca207e384452d3271620fb
unknown <lassehansencphbusiness@gmail.com>
2018-10-25 18:05:23 +0200

    Add user test

---
+++ LSDProjectBackend/src/test/java/testQueries/UserTestIT.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testQueries;
+
+import DBLayer.UserQueries;
+import entities.User;
+import java.sql.SQLException;
+import org.junit.Test;
+
+/**
+ *
+ * @author Lasse
+ */
+public class UserTestIT {
+
+    @Test
+    public void insertUser() throws SQLException {
+        UserQueries uq = new UserQueries();
+
+        User u1 = new User(1, "user", "poobread", "abcdef", "I am a user");
+        User u2 = new User(2, "user", "einstein", "dasdas", "I am clever");
+
+        uq.insertUser(u1);
+        uq.insertUser(u2);
+    }
+}
