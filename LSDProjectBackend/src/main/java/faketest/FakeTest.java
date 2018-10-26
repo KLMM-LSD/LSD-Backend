4ed3d0d5242f145d931f6d8407469b3c722985db
unknown <lassehansencphbusiness@gmail.com>
2018-10-26 14:03:47 +0200

    generate json for frontpage + tests

--- LSDProjectBackend/src/main/java/faketest/FakeTest.java
+++ LSDProjectBackend/src/main/java/faketest/FakeTest.java
@@ -19,68 +19,30 @@ import java.util.ArrayList;
  * @author Lasse
  */
 public class FakeTest {
-
-    public static void initUsers() throws SQLException {
-        UserQueries uq = new UserQueries();
-        PostQueries pq = new PostQueries();
-
-        Post p1 = new Post();
-        Post p2 = new Post();
-        Post p3 = new Post();
-        Post p4 = new Post();
-        Post p5 = new Post();
-
-        User u1 = new User(-1, "user", "poobread", "abcdef");
-        User u2 = new User(-1, "user", "einstein", "dasdas");
-
-        p1.initStory(1, 1, "Story quite a story");
-        p2.initComment(2, 1, 2, "Comment good story");
-        p3.initComment(3, 2, 1, "comment thanks dude");
-
-        p4.initStory(4, 2, "Story I am drunk");
-        p5.initComment(5, 4, 1, "Commetn goos story");
-
-        uq.insertUser(u1);
-        uq.insertUser(u2);
-
-        pq.insertStory(p1);
-        pq.insertCommentWithLookup(p2);
-        pq.insertCommentWithLookup(p3);
-
-        pq.insertStory(p4);
-        pq.insertCommentWithLookup(p5);
-
-    }
-
+    
     public static void testGet() throws SQLException {
         PostQueries pq = new PostQueries();
-        ArrayList<Post> ret = pq.getThread(1);
-        JsonObject list = new JsonObject();
+        JsonObject ret = new JsonObject();
 
-        JsonArray arr_postids = new JsonArray();
-        JsonArray arr_postparentid = new JsonArray();
+        JsonArray arr_postid = new JsonArray();
         JsonArray arr_postauthorid = new JsonArray();
-        JsonArray arr_postthreadid = new JsonArray();
         JsonArray arr_postcontent = new JsonArray();
 
-        for (Post p : ret) {
-            arr_postids.add(p.postid);
-            arr_postparentid.add(p.postparentid);
-            arr_postauthorid.add(p.postauthorid);
-            arr_postthreadid.add(p.postthreadid);
-            arr_postcontent.add(p.postcontent);
+        ArrayList<Post> list = pq.getMostRecentStories();
+
+        for (Post tmp : list) {
+            arr_postid.add(tmp.postid);
+            arr_postauthorid.add(tmp.postauthorid);
+            arr_postcontent.add(tmp.postcontent);
         }
-        
-        
-        list.addProperty("length", ret.size());
 
-        list.add("arr_postids", arr_postids);
-        list.add("arr_postparentid", arr_postparentid);
-        list.add("arr_postauthorid", arr_postauthorid);
-        list.add("arr_postthreadid", arr_postthreadid);
-        list.add("arr_postcontent", arr_postcontent);
+        ret.add("arr_postid", arr_postid);
+        ret.add("arr_postauthorid", arr_postauthorid);
+        ret.add("arr_postcontent", arr_postcontent);
 
-        System.out.println(list.toString());
+        ret.addProperty("len", ret.size());
+        
+        System.out.println(ret.toString());
     }
 
     public static void main(String[] args) throws SQLException {
--- LSDProjectBackend/src/main/java/service/PostResource.java
+++ LSDProjectBackend/src/main/java/service/PostResource.java
@@ -1,58 +1,65 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package service;
-//
-//import entities.Posts;
-//import java.sql.SQLException;
-//import javax.json.JsonObject;
-//import javax.ws.rs.core.Context;
-//import javax.ws.rs.core.UriInfo;
-//import javax.ws.rs.Consumes;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.core.MediaType;
-//
-///**
-// * REST Web Service
-// *
-// * @author Micha
-// */
-//@Path("/posts")
-//public class PostResource {
-//    
-//    @Context
-//    private UriInfo context;
-//
-//    /**
-//     * Creates a new instance of PostResource
-//     */
-//    public PostResource() {
-//    }
-//
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public String getPost() {
-//        String b = "Hello there!";
-//        return b;
-//    }
-//    
-//    @POST
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public void postPost(JsonObject js) throws SQLException, ClassNotFoundException {
-//        PostQueries postQ = new PostQueries();
-//        Posts post = new Posts();
-//        post.setPostid(js.getInt("postid"));
-//        post.setPosttype(js.getString("posttype"));
-//        post.setPostparentid(js.getInt("postparentid"));
-//        post.setPosttimestamp(System.currentTimeMillis());
-//        post.setPostauthorid(js.getInt("postauthorid"));
-//        post.setPostcontent(js.getString("postcontent"));
-//        postQ.createPost(post);
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import DBLayer.PostQueries;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import entities.Post;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Lasse
+ */
+@Path("/posts")
+public class PostResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of PostResource
+     */
+    public PostResource() {
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getFrontPageThreads() throws SQLException {
+        PostQueries pq = new PostQueries();
+        JsonObject ret = new JsonObject();
+
+        JsonArray arr_postid = new JsonArray();
+        JsonArray arr_postauthorid = new JsonArray();
+        JsonArray arr_postcontent = new JsonArray();
+
+        ArrayList<Post> list = pq.getMostRecentStories();
+
+        for (Post tmp : list) {
+            arr_postid.add(tmp.postid);
+            arr_postauthorid.add(tmp.postauthorid);
+            arr_postcontent.add(tmp.postcontent);
+        }
+
+        ret.add("arr_postid", arr_postid);
+        ret.add("arr_postauthorid", arr_postauthorid);
+        ret.add("arr_postcontent", arr_postcontent);
+
+        ret.addProperty("len", ret.size());
+
+        return ret.toString();
+    }
+
+}
