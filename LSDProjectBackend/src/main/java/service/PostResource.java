974e53bb60f08f65838b9c92dc273bbc07e59eb3
unknown <lassehansencphbusiness@gmail.com>
2018-10-26 20:38:43 +0200

    rest api for thread/post lookup

--- LSDProjectBackend/src/main/java/service/PostResource.java
+++ LSDProjectBackend/src/main/java/service/PostResource.java
@@ -11,12 +11,16 @@ import com.google.gson.JsonObject;
 import entities.Post;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -36,16 +40,16 @@ public class PostResource {
     }
 
     @GET
+    @Path("frontpage")
     @Produces(MediaType.APPLICATION_JSON)
     public String getFrontPageThreads() throws SQLException {
         PostQueries pq = new PostQueries();
         JsonObject ret = new JsonObject();
 
-        JsonArray arr_postid = new JsonArray();
-        JsonArray arr_postauthorid = new JsonArray();
-        JsonArray arr_postcontent = new JsonArray();
-
         ArrayList<Post> list = pq.getMostRecentStories();
+        JsonArray arr_postid = new JsonArray(list.size());
+        JsonArray arr_postauthorid = new JsonArray(list.size());
+        JsonArray arr_postcontent = new JsonArray(list.size());
 
         for (Post tmp : list) {
             arr_postid.add(tmp.postid);
@@ -62,4 +66,47 @@ public class PostResource {
         return ret.toString();
     }
 
+    /* TODO: ThreadID */
+    @GET
+    @Path("thread")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getThreadPosts(@QueryParam("id") int threadid) {
+        PostQueries pq = new PostQueries();
+        JsonObject ret = new JsonObject();
+        ArrayList<Post> list;
+
+        try {
+            list = pq.getThread(threadid);
+        } catch (SQLException ex) {
+            Logger.getLogger(PostResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response.status(500).build();
+        }
+
+        JsonArray arr_postid = new JsonArray(list.size());
+        JsonArray arr_postparentid = new JsonArray(list.size());
+        JsonArray arr_postauthorid = new JsonArray(list.size());
+        JsonArray arr_postcontent = new JsonArray(list.size());
+
+        for (Post p : list) {
+            arr_postid.add(p.postid);
+            arr_postparentid.add(p.postparentid);
+            arr_postauthorid.add(p.postauthorid);
+            arr_postcontent.add(p.postcontent);
+        }
+
+        ret.addProperty("len", list.size());
+
+        if (!list.isEmpty() && list.get(0).posttype.equals("story")) {
+            ret.addProperty("first_is_story", true);
+        } else {
+            ret.addProperty("first_is_story", false);
+        }
+
+        ret.add("arr_postid", arr_postid);
+        ret.add("arr_postparentid", arr_postparentid);
+        ret.add("arr_postauthorid", arr_postauthorid);
+        ret.add("arr_postcontent", arr_postcontent);
+
+        return Response.ok(ret.toString()).build();
+    }
 }
